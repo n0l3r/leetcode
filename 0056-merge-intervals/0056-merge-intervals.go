833a18875e445bbf3ad0aa7980f1c946e88d0f66
Naufal Taufiq Ridwan <71002775+n0l3r@users.noreply.github.com>
2024-06-27 16:16:21 +0700

    Time: 17 ms (56.71%), Space: 6.5 MB (38.85%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -0,0 +1,19 @@
+// Source: https://leetcode.com/problems/merge-intervals/
+func merge(intervals [][]int) [][]int {
+	if len(intervals) == 0 {
+		return [][]int{}
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	var res [][]int
+	res = append(res, intervals[0])
+	for i := 1; i < len(intervals); i++ {
+		if res[len(res)-1][1] >= intervals[i][0] {
+			res[len(res)-1][1] = max(res[len(res)-1][1], intervals[i][1])
+		} else {
+			res = append(res, intervals[i])
+		}
+	}
+	return res
+}

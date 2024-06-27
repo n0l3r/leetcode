4b1b50420a0ef53d458817d88847767f83fc7989
Naufal Taufiq Ridwan <71002775+n0l3r@users.noreply.github.com>
2024-06-27 15:42:00 +0700

    Time: 6 ms (68.85%), Space: 4.3 MB (30.81%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{j, i}
+		}
+		m[v] = i
+	}
+	return nil
+}

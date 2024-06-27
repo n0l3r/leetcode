6f8dc282d240d05e67bb255743f9e4714bff10bb
Naufal Taufiq Ridwan <71002775+n0l3r@users.noreply.github.com>
2024-06-27 16:14:50 +0700

    Time: 0 ms (100%), Space: 2.4 MB (58.81%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.go
@@ -0,0 +1,13 @@
+// Source: https://leetcode.com/problems/rotate-image/
+func rotate(matrix [][]int)  {
+	for i := 0; i < len(matrix); i++ {
+		for j := i; j < len(matrix); j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix)/2; j++ {
+			matrix[i][j], matrix[i][len(matrix)-1-j] = matrix[i][len(matrix)-1-j], matrix[i][j]
+		}
+	}
+}

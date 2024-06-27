83263b7a9ad430959a2cfc2e48c37880875fde1b
Naufal Taufiq Ridwan <71002775+n0l3r@users.noreply.github.com>
2024-06-27 16:06:58 +0700

    Time: 7 ms (82.47%), Space: 2.9 MB (83.62%) - LeetHub

---
+++ 0018-4sum/0018-4sum.go
@@ -0,0 +1,35 @@
+// Source: https://leetcode.com/problems/4sum/
+func fourSum(nums []int, target int) [][]int {
+	sort.Ints(nums)
+	var res [][]int
+	for i := 0; i < len(nums)-3; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		for j := i + 1; j < len(nums)-2; j++ {
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+			l, r := j+1, len(nums)-1
+			for l < r {
+				sum := nums[i] + nums[j] + nums[l] + nums[r]
+				if sum == target {
+					res = append(res, []int{nums[i], nums[j], nums[l], nums[r]})
+					for l < r && nums[l] == nums[l+1] {
+						l++
+					}
+					for l < r && nums[r] == nums[r-1] {
+						r--
+					}
+					l++
+					r--
+				} else if sum < target {
+					l++
+				} else {
+					r--
+				}
+			}
+		}
+	}
+	return res
+}
